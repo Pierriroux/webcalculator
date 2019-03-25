17a91598813c287d490c9a914354b53ef058cec2
Pierriroux <pierrekouliche@hotmail.fr>
2019-03-25 09:24:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
+<form action="index.php" method="post">
+Name: <input type="text" name="name">
+<input type="submit">
+</form>
+<?php
+$filename = 'friends.txt';
+$file = fopen( $filename, "r" );
+while (!feof($file)) {
+    // reading file
+}
 
+// appending to file
+$file = fopen( $filename, "a" );
+fwrite( $file, "some string" );
+?>
