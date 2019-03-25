445c535524666b75e96e4ae9e36be4434bde71d6
Pierriroux <pierrekouliche@hotmail.fr>
2019-03-25 09:14:42 +0100

    Add files via upload

---
+++ labWeb/labWeb1.php
@@ -0,0 +1,9 @@
+<?php
+$names = array("Patrick", "John", "Marie", "Claire", "Paul");
+foreach ($names as $name) {
+    if(strpos($name, 'Pa')===0)
+      {
+        print "$name, ";
+      }
+}
+?>
---
+++ labWeb/labWeb2.php
@@ -0,0 +1,21 @@
+<?php
+function sum($x, $y) {
+	return $x + $y;
+}
+
+function sub($x, $y) {
+	return $x - $y;
+}
+
+function mult($x, $y) {
+	return $x * $y;
+}
+
+function div($x, $y) {
+	return $x / $y;
+}
+echo (sum(5, 7) . "\n ");
+echo (sub(5, 7) . "\n ");
+echo (mult(5, 7) . "\n ");
+echo (div(5, 7) . "\n ");
+?>
