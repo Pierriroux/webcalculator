8ab532de1b1a17a07d77047e0508d3d5b12c3100
Pierriroux <pierrekouliche@hotmail.fr>
2019-04-05 23:30:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,30 +14,7 @@ if(isset($_POST["name"]))
 while (!feof($file)) 
 {
     $word = fgets($file);
-    //echo "<li>".$word."</li>";
-/*	$word=fgets($file);
-	if(isset($_GET["nameFilter"]))
-	{
-		if (isset($_GET["startingWith"]))
-		{
-			$pos = strpos(strtolower ($word), strtolower ($_GET["nameFilter"]));
-			if ($pos !== false) 
-			{
-				if($pos===0)
-				echo "<li>".$word."</li>";
-			}
-			
-		}
-		else
-		{
-			if(strstr(strtolower ($word),strtolower ($_GET["nameFilter"])))
-				{echo "<li>".$word."</li>";}
-		}
-	}
-	else
-	{
-		echo "<li>".$word."</li>";
-    }*/
+    echo "<li>".$word."</li>";
 }
 fclose($file);
 ?>
