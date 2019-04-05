ef66c446a95ee0cd6c21bec41e02087ed85fedd5
Pierriroux <pierrekouliche@hotmail.fr>
2019-04-05 23:20:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,11 @@
 <?php
 $filename = 'friend.txt';
 $file = fopen( $filename, "r" );
-	if(isset($_POST["name"]))
-{	if($_POST["name"]!=""){
+if(isset($_POST["name"]))
+{
 	$file = fopen( $filename, "a" );
 	fwrite( $file, PHP_EOL.$_POST["name"] );
 	fclose($file);
-}
 }
 while (!feof($file)) 
 {
