2d810267f1580a4c2e0ad9fcc457e55b1d8a3e03
Pierriroux <pierrekouliche@hotmail.fr>
2019-04-05 22:37:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,28 +3,31 @@
     <input type="submit">
 </form>
 <?php
-$filename = 'friends.txt';
-/*if(isset($_POST["name"]))
-{
-    if($_POST["name"]!="")
-    {
-        $file = fopen( $filename, "a" );
-        fwrite( $file, PHP_EOL.$_POST["name"] );
-        fclose($file);
-    }
+$file = fopen( $filename, "r" );
+while (!feof($file)) {
+	$word=fgets($file);
+	if(isset($_GET["nameFilter"]))
+	{
+		if (isset($_GET["startingWith"]))
+		{
+			$pos = strpos(strtolower ($word), strtolower ($_GET["nameFilter"]));
+			if ($pos !== false) 
+			{
+				if($pos===0)
+				echo "<li>".$word."</li>";
+			}
+			
+		}
+		else
+		{
+			if(strstr(strtolower ($word),strtolower ($_GET["nameFilter"])))
+				{echo "<li>".$word."</li>";}
+		}
+	}
+	else
+	{
+		echo "<li>".$word."</li>";
+	}
 }
-*/
-// appending to file
-$file = fopen( $filename, "a" );
-fwrite( $file, "some string" );
-$friendsArray = array();
-if($file != false){
-    while(!feof($file)){
-        $name = trim(fgets($file));
-        if(strlen($name)>0) {
-            $friendsArray[] = $name;
-        }
-    }
-    fclose($file);
-}
+fclose($file);
 ?>
